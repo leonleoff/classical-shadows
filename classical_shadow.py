7e7fd9324c380127024133e279edb59aaeefa359
leonleoff <76099309+leonleoff@users.noreply.github.com>
2025-11-29 18:39:30 +0100

    wip: new architec

--- classical_shadow.py
+++ classical_shadow.py
@@ -8,17 +8,14 @@ from backrotation import (
     n_qubit_clifford_backrotation,
     single_qubit_clifford_backrotation,
 )
-from random_clifford import get_random_1qbit_clifford, get_random_nqbit_clifford
-
-# TODO: maybe refactor beacause when nqbit clifford are added
+from random_clifford import get_random_nqbit_clifford, get_random_rotations
 
 
-class ClassicalShadow(ABC):
-    def __init__(self, num_qubits: int, single_qubit_clifford: bool = True):
+class AbstractClassicalShadow(ABC):
+    def __init__(self, num_qubits: int):
         self.num_qubits: int = num_qubits
-        self.single_qubit_clifford: bool = single_qubit_clifford
 
-        self.snapshots: list[list[np.ndarray]] = []
+        self.snapshots = []  # no type beacuse can be denstiy matrix or stabilzer
 
     def get_desity_matrix_from_snapshots(self):
         print("generating density matrix from snapshots")
@@ -27,22 +24,51 @@ class ClassicalShadow(ABC):
         print("getting original destiny matrix from state creation circuit")
 
     def add_snapshot(self):
-        # get random rotation
-        if self.single_qubit_clifford:
-            rotations: list[str] = get_random_1qbit_clifford(self.num_qubits)
-        else:
-            rotations: list[str] = get_random_nqbit_clifford(self.num_qubits)
+        rotations: list[str] = self.get_random_rotations(self.num_qubits)
 
+        # prepare circuit
         state_circuit: QuantumCircuit = self.get_state_circuit()
         combined_circuit: QuantumCircuit = self.make_rotated_state_ciruit(
             rotations, state_circuit
         )
 
-        # getting the measurement results for each qubit
+        # run circuit and getting the measurement results for each qubit
         measurement_results = self.run_cuircuit_and_get_measurment(combined_circuit)
+        assert len(measurement_results) == self.num_qubits
 
+        # roatet back and store snapshot
         snapshot = self.compute_snapshot(rotations, measurement_results)
         self.snapshots.append(snapshot)
+
+    def predict_observable(self):
+        raise NotImplementedError("This function is not yet implemented.")
+
+    def make_rotated_state_ciruit(
+        self, rotation_description, state_creation_circuit
+    ) -> QuantumCircuit:
+        print("checking if qbit count is the same ")
+        print("makes a circuit out of description")
+        print("combining both circuits")
+
+    @abstractmethod
+    def compute_snapshot(self, rotation_description, measurement):
+        raise NotImplementedError("This method should be implemented by subclasses")
+
+    @abstractmethod
+    def run_cuircuit_and_get_measurment(self, circuit):
+        raise NotImplementedError("This function is not yet implemented.")
+
+    @abstractmethod
+    def get_state_circuit(self) -> QuantumCircuit:
+        """ "Returns the quantum circuit that prepare the state of interest."""
+        raise NotImplementedError("This method should be implemented by subclasses")
+
+    @abstractmethod
+    def get_random_rotations(self, num_qubits) -> list[str]:
+        raise NotImplementedError("This method should be implemented by subclasses")
+
+
+class ClassicalShadow_SINGLE_CLIFFORD(AbstractClassicalShadow):
 
     def predict_observable(self):
         raise NotImplementedError("This function is not yet implemented.")
--- random_clifford.py
+++ random_clifford.py
@@ -5,5 +5,5 @@ def get_random_nqbit_clifford(num_qubits) -> list[str]:
     raise NotImplementedError("This function is not yet implemented.")
 
 
-def get_random_1qbit_clifford(num_qubits) -> list[str]:
+def get_random_rotations(num_qubits) -> list[str]:
     return [random.choice(["Z", "H", "X"]) for i in range(num_qubits)]
