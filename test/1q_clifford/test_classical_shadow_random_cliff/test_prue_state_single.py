521622fa8e6ca4c0dc0d5c93f715325d995fe68a
leonleoff <76099309+leonleoff@users.noreply.github.com>
2025-12-06 13:55:52 +0100

    bigger lim

--- test/1q_clifford/test_classical_shadow_random_cliff/test_prue_state_single.py
+++ test/1q_clifford/test_classical_shadow_random_cliff/test_prue_state_single.py
@@ -52,6 +52,6 @@ def test_reconstruction_with_identity():
         np.real(reconstructed_dm),
         expected_matrix,
         rtol=0.0,
-        atol=0.05,
+        atol=0.08,
         err_msg="The reconstructed density matrix differs by more than 0.01 from the expected matrix.",
     )
